cac39ac54cf84a0a935cd24f0448eb54adf84a8e
Maia <zgovorovski@gmail.com>
2020-09-16 11:53:00 +0100

    Better formatting for LandingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import styles from './FullPage.module.css'; 
-import MOTD from '../components/MOTD'
+import styles from './FullPage.module.css';
+import MOTD from '../components/MOTD';
 
 export default function Page() {
-    
-
-    return (
-        <div className={styles.landingPage}>
-            <div className={styles.centerContent}>
-                <p className={styles.title}>Hello! 💕</p>
-                <p className={styles.subtitle}><MOTD /></p>
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.landingPage}>
+			<div className={styles.centerContent}>
+				<p className={styles.title}>Hello! 💕</p>
+				<div className={styles.subtitle}>
+					<MOTD />
+				</div>
+			</div>
+		</div>
+	);
 }
